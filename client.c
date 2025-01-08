8fdff8edebb3ceaac976a20969c3fb7e35483a85
jsonano <jchao5768@gmail.com>
2025-01-08 00:18:48 -0500

    Added comments and devlog update

--- client.c
+++ client.c
@@ -16,15 +16,19 @@ int main() {
     char * current_word;
     char user_typed_word[BUFFER_SIZE];
     while (current_word = strsep(&remaining_string, " ")) {
+        // Prevents printing (null) when remaining_string is NULL (i.e. last word).
         if (remaining_string == NULL) {
             printf("\033[1;31m%s\033[0m\n", current_word);
         } else {
             printf("\033[1;31m%s\033[0m %s\n", current_word, remaining_string); // red, bolded text (use 4;31m if bold doesn't work)
         }
+
+        // Initial prompt for user, without testing.
         printf("Type: %s\n", current_word);
         printf(">> ");
         fgets(user_typed_word, BUFFER_SIZE, stdin);
 
+        // If initial input is incorrect, continue requesting input until correct.
         while (check_word(current_word, user_typed_word) == 0) {
             printf("\n\nIncorrect. Try again.\n");
             printf("Type: %s\n", current_word);
@@ -42,7 +46,9 @@ int check_word(char * word, char * typed_word) {
     printf("\n"); // for formatting purposes
 
     // PROBLEM: if typed word is correct, but has extra characters, it will still be correct
+    
     int correct_status = 1;
+    // Check each character of the word and prints w/ appropriate background color.
     for (int i = 0; i < strlen(word); i++) {
         char temp_string[BUFFER_SIZE];
         if (word[i] == typed_word[i]) {
