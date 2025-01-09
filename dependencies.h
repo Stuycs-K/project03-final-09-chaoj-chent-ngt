993139468468dfcd8e26ad4114709fc7109a1880
muff1n0 <tchen51@stuy.edu>
2025-01-09 14:26:22 -0500

    fix typo

--- dependencies.h
+++ dependencies.h
@@ -14,13 +14,10 @@
 #define PORT 8080
 #define BUFFER_SIZE 256
 
-<<<<<<< HEAD
 void client_connect(int * clientSocket);
 void server_connect(int * sockfd, struct sockaddr_in * serverAddr);
 
 int err();
-=======
-int err();
+
 
 int check_word(char * word, char * typed_word);
->>>>>>> main
